dfdf040c38805f90ae52acc3b6d13e508bab63c4
Vasini25 <flavippolini@yahoo.com.br>
2023-06-03 15:26:22 -0300

    adicionado template para pesquisa por filtro em HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,22 @@
 <template>
-  <v-container>
-    <v-btn>TESTE</v-btn>
-  </v-container>
+  <v-card width="2000" class="mx-auto mt-5">
+    <v-card-title>
+      <h1>Filtro</h1>
+    </v-card-title>
+
+    <v-card-text>
+      <v-form>
+        <v-text-field label="Pesquisar" 
+        hint="Digite uma palavra pela qual filtrar" 
+        prepend-icon="mdi-magnify"></v-text-field>
+      </v-form>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn>Remover Filtros</v-btn>
+      <v-btn color="blue" >Filtrar</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
@@ -9,56 +24,6 @@
     name: 'HelloWorld',
 
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
     }),
   }
 </script>
