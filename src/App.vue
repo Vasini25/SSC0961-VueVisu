073afb5c6445691c6ad6da9d92c8b209d0d5277f
Mateus Andrade <sccpjoaopedro@gmail.com>
2023-06-04 18:33:50 -0300

    added header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,89 @@
 <template>
   <v-app>
-    <!--APLICAÇÃO AQUI-->
-    <!--
-    <v-card width="400" class="mx-auto mt-5">
-      <v-card-title>
-        <h1>Login</h1>
-      </v-card-title>
+      <CHeader>
+        <div class="container" style="margin-left: 10px;">
+          <v-img src="@/assets/icmc.jpg" alt="logo ICMC" contain height="100px" width="250px">
+          </v-img>
+        </div>
+      </CHeader>
+      <!--APLICAÇÃO AQUI-->
+      <!--
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title>
+          <h1>Login</h1>
+        </v-card-title>
 
-      <v-card-text>
-        <v-form>
-          <v-text-field label="Username" prepend-icon="mdi-account-circle"></v-text-field>
-        </v-form>
-      </v-card-text>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Username" prepend-icon="mdi-account-circle"></v-text-field>
+          </v-form>
+        </v-card-text>
 
-      <v-card-actions>
-      <v-btn color="blue" >Teste</v-btn>
-      <v-btn>teste</v-btn>
-    </v-card-actions>
-    </v-card>
+        <v-card-actions>
+        <v-btn color="blue" >Teste</v-btn>
+        <v-btn>teste</v-btn>
+      </v-card-actions>
+      </v-card>
 
-      <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+        <v-app-bar
+        app
+        color="primary"
+        dark
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <div class="d-flex align-center">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+          />
 
-  -->
+          <v-img
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+            width="100"
+          />
+        </div>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          href="https://github.com/vuetifyjs/vuetify/releases/latest"
+          target="_blank"
+          text
+        >
+          <span class="mr-2">Latest Release</span>
+          <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+      </v-app-bar>
+
+    -->
     <v-main>
       <HelloWorld/>
       <GetRequisition/>
     </v-main>
+    <v-footer class="bg-grey-lighten-1">
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          variant="text"
+          class="mx-2"
+          rounded="xl"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col class="text-center mt-4" cols="12">
+          ({{ new Date().getFullYear() }}) <strong>SSC0961 - Desenvolvimento Web e Mobile</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
