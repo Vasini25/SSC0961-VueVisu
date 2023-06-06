34d38c640ef34496dc199a1002a377d2b5bd3d9a
Luccas Stroppa Saliba Barizon <77598610+luccassaliba@users.noreply.github.com>
2023-06-05 23:18:05 -0300

    Create router.js

---
+++ src/routers/router.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "@/App.vue";
+import HomePage from "@/views/HomePage.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomePage,
+  },
+  {
+    path: "/app",
+    name: "app",
+    component: App,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
